047e4aa9b1055087ec9ececac452be82049c4670
serra99 <33655921+serra99@users.noreply.github.com>
2019-12-17 21:57:39 +0100

    Add files via upload

--- E02/main.c
+++ E02/main.c
@@ -1,119 +1,115 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define N 4
-#define FILENAME "e2_test_set_PROG.txt"
+#define TEST_FILE "e2_test_set_PROG.txt"
+#define N_PIETRE 4
+typedef enum{
+    p_zaffiri, p_rubini, p_topazi, p_smeraldi, p_pietre
+} e_pietre;
 
-int disp_ripet(int pos, int *dispGemme, int *sol, int n, int k, int prec);
-int verifica(int *sol, int k);
-char gemme[N] = {'Z', 'R', 'T', 'S'};
-int main(void){
-  int  i, s, k, numGemme[N], dispGemme[N], maxlun, bestlun, numtestset, min, max;
-  //char gemme[N] = {'Z', 'R', 'T', 'S'};
-  int *sol;
-  int rip = 0;
-  int *val_pietre,*npietre;
-  int val_c, val_max, max_rip;
-  val_max = 0;
-  val_c = 0;
-  FILE *in = fopen(FILENAME, "r");
-  if (in == NULL)
-    return 0;
+char* gemme[N_PIETRE] = {"Z", "R", "T", "S"}; // vettore parallelo a valori e a disp_pietre
+int valori[N_PIETRE]; //variabile globale in cui sono contenuti i valori di ogni pietra
 
-  fscanf(in, "%d", &numtestset);
-  /* INIZIO TEST*/
-  for (s=0; s<numtestset; s++) {
+void generaCollane(int *val, int len, int max_rip); // funzione wrapper che va dalla lunghezza massima a 0
+int disp_rip(int pos,int prec, int *val, int len, int *sol, int k, int *bestSol, int *bestSolLen, int *bestSolVal,int max_rip);
+int verifica(int *sol, int len); //return il valore della collana, se ci sono ripetizioni di zaffiri superiori agli smeraldi return -1;
 
-    printf("Test set #%d\n", s+1);
-    min = 0;
-    maxlun = 0;
+int main(){
+    int pietre[N_PIETRE];
+    int i,j,n_test,max_rip;
 
-    val_pietre = malloc(N * sizeof(int));
-    npietre = calloc(N,sizeof(int));
-    for (i=0; i<N; i++) {// maxlun = z + s + t + r;
-      fscanf(in, "%d", &numGemme[i]);
-      maxlun += numGemme[i];
+    FILE* f_test;
+    //apertura del file di test
+    f_test = fopen(TEST_FILE, "r");
+    if(f_test == NULL) {
+        printf("Impossibile aprire il file di TEST\n");
+        exit(-1);
     }
-    for(i = 0; i < N; i++)
-        fscanf(in, "%d", &val_pietre[i]);
-    fscanf(in,"%d",&max_rip);
-    max = maxlun;
-    sol = malloc((maxlun) * sizeof(int));
-    printf("Prima del while...\n");
-    printf("MAX_RIP : %d\n",max_rip);
+    ///TEST
+    fscanf(f_test, "%d",&n_test);
+    for(j = 0; j < n_test; j++){
+        printf("\n\n\nTEST #%d\n",j+1);
+        for(i = 0; i < N_PIETRE; i++){
+            fscanf(f_test,"%d",&pietre[i]);
+            printf("%s: %d\n",gemme[i],pietre[i]);
+        }
+        for(i = 0; i < N_PIETRE; i++)
+            fscanf(f_test,"%d",&valori[i]);
+        fscanf(f_test, "%d",&max_rip);
 
-    while (min <= max) {
-        int rip = 0;
-      k = min + (max-min)/2;
-      for (i=0; i<N; i++)
-        dispGemme[i]=numGemme[i];
-
-      if (disp_ripet(0, dispGemme, sol, N, k, -1)) {
-
-        if(val_c > val_max)
-            val_c = val_max;
-        bestlun = k;
-        min = k+1;
-    } else
-        max = k-1;
-
+        generaCollane(pietre,N_PIETRE,max_rip);
     }
-    printf("Una soluzione a lunghezza massima %d e' ",bestlun);
-    for (i=0; i<bestlun; i++)
-      printf("%c", gemme[sol[i]]);
-    printf("\n");
-    //exit(1);
+}
+
+void generaCollane(int *val, int len,int max_rip){
+    int i = 0;
+    int *sol;
+    int *bestSol;
+    int bestSolLen = 0;
+    int bestSolVal = 0;
+    int max_len = 0;
+    for(i = 0; i < len; i++){
+        max_len += val[i];
+    }
+    sol = malloc(max_len* sizeof(int));
+    bestSol = malloc(max_len* sizeof(int));
+    for(i = max_len; i > 0; i--){
+        disp_rip(0, -1, val, len, sol, i, bestSol, &bestSolLen, &bestSolVal,max_rip);
+    }
+    for(i = 0; i < bestSolLen; i++){
+        printf("%s", gemme[bestSol[i]]);
+    }
+    printf("\nLUNGHEZZA TROVATA: %d\nVALORE: %d",  bestSolLen, bestSolVal);
     free(sol);
-
-  }
-
-  /*FINE DEL TEST*/
-
-  fclose(in);
-  return 0;
+    free(bestSol);
 }
 
-int controlla(int *sol, int n){
-    int i;
-    int n_z = 0, n_s = 0;
-    int rip = 0;
-    printf("SOL PARZIALE: \n");
+int disp_rip(int pos,int prec, int *disp_gemme, int len, int *sol, int k, int *bestSol, int *bestSolLen, int *bestVal,int max_rip){
+    int i, j, count, curr_val;
 
-    for(i = 1; i < n; i++){
-        if(sol[i] == 0) n_z++;
-        else if(sol[i] == 3) n_s++;
+    if(pos>=k){
+        if((curr_val = verifica(sol, k)) != -1){
+            if(curr_val > *bestVal){
+                *bestVal = curr_val;
+                *bestSolLen = k;
+                for(i = 0; i < k; i++){
+                    bestSol[i] = sol[i];
+                }
+            }
+            return 1;
+        }
+
     }
-    if(n_z <= n_s) return 1;
+
+    for(i = 0; i < len; i++){
+        if(prec != -1){
+            if((sol[prec] == p_zaffiri ||  sol[prec] == p_topazi) && (i != p_zaffiri && i != p_rubini)) //caso incompatibile
+                    continue;
+            if((sol[prec] == p_smeraldi || sol[prec] == p_rubini) && (i != p_smeraldi && i != p_topazi))//caso incompatibile
+                    continue;
+            for(j = prec, count = 1; sol[j] == i && count < max_rip+1; j--, count++); //se una pietra è ripetuta più di max_rip volte, uso un'altra pietra
+            if(count > max_rip) continue;
+        }
+        if(disp_gemme[i] > 0){
+            disp_gemme[i]--;
+            sol[pos] = i;
+            if(disp_rip(pos+1,pos, disp_gemme, len, sol, k, bestSol, bestSolLen, bestVal,max_rip))
+                return 1;
+            disp_gemme[i]++;
+        }
+    }
     return 0;
 }
-int disp_ripet(int pos, int *dispGemme, int *sol, int n, int k, int prec) {
-  int i;
-  if (pos >= k) {
-    return 1;
-  }
 
-  for (i = 0; i < n; i++){ // i rappresenta la pietra che voglio posare
-
-    if (dispGemme[i] > 0  ) { // se ci sono ancora gemme disponibili di quel tipo
-      if (prec != -1) { // se ho già posato una pietra
-        if (((sol[prec]==0) || (sol[prec]==2)) && (i!=0) && (i!=1)) //se in sol[prec] = Z o T e i != Z e R
-          continue; // NON assegno la pietra i
-        if (((sol[prec]==3) || (sol[prec]==1)) && (i!=2) && (i!=3))//se in sol[prec] = S o R e i != T e S
-          continue; // NON assegno la pietra i
-        //printf("PIETRA : %d PREC: %d rip: %d\n",i,sol[prec],rip);
-      }
-        /*POSO LA PIETRA se non supero gli if*/
-
-          //printf("%d %d\n",rip,max_rip);
-    dispGemme[i]--; //riduco la quantita della i-esima pietra
-    sol[pos] = i; //assegno la pietra
-
-    if (disp_ripet(pos+1, dispGemme, sol, n, k, pos)){ //ricorro
-        return 1;
-     }
-     dispGemme[i]++; //backtrack
-
-
+int verifica(int *sol, int len){
+    int *occ;
+    int val_tot = 0;
+    int i;
+    occ = calloc(N_PIETRE, sizeof(int));
+    for(i = 0; i < len; i++){
+        occ[sol[i]]++;
     }
-  }
-  return 0;
+    for(i = 0; i < N_PIETRE; i++) val_tot += occ[i]*valori[i];
+    if(occ[p_zaffiri] > occ[p_smeraldi]) val_tot = -1;
+    free(occ);
+    return val_tot;
 }
